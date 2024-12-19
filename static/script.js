50f8cd03237b22fa2dd0b4cf518fe43f656be282
Atig-Hamza <hamzaatig@icloud.com>
2024-12-19 17:07:54 +0100

    homepage: create smooth custome cursor

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+//smooth scroll
 const lenis = new Lenis({
     smooth: true,
     lerp: 0.12,
@@ -10,3 +11,39 @@ const lenis = new Lenis({
   
   requestAnimationFrame(raf);
   
+//cursor edit
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
