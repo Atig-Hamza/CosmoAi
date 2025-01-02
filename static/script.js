7a68d3c5390bc53597a4937670c5bbc88b7be86b
Atig-Hamza <hamzaatig@icloud.com>
2025-01-02 10:26:50 +0100

    Homepage: change full section content and design to new one, Uhistory.html: change font

--- static/script.js
+++ static/script.js
@@ -118,41 +118,108 @@ for(const star of document.getElementsByClassName("magic-star")) {
 }
 
 //section animation
-let panels = gsap.utils.toArray(".card__content");
+gsap.set(".circle", { yPercent: -5 });
+    gsap.set(".dotsBlue", { yPercent: 10 });
+    gsap.set(".owlHorned", { yPercent: -20 });
+    gsap.set(".clusterGreat", { yPercent: 5 });
 
-panels.forEach((item, i) => {
-  const contentElements = item.querySelectorAll(".card__inner > *");
+    gsap.to(".circle", {
+      yPercent: 5,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterGreat",
+        scrub: 1
+      },
+    });
 
-  gsap.set(contentElements, {
-    y: 0,
-    opacity: 0
-  });
+    gsap.to(".dotsBlue", {
+      yPercent: -10,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterGreat",
+        scrub: 1
+      },
+    });
 
-  
-  ScrollTrigger.create({
-    trigger: item,
-    
-    pin: true,
-    start: "top 50%",
-    end: "bottom 50%",
-    
-    snap: { snapTo: [0.5], duration: 1, delay: 0},
-    
-    onEnter: ({ progress, direction, isActive }) => {
-      console.log("onEnter", progress, direction, isActive);
-      gsap.fromTo(contentElements, { y: 80, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.05 });
-    },
-    onLeave: ({ progress, direction, isActive }) => {
-      console.log("onLeave", progress, direction, isActive);
-      gsap.fromTo(contentElements, { y: 0, opacity: 1 }, { y: -80, opacity: 0, stagger: 0.05 });
-    },
-    onLeaveBack: ({ progress, direction, isActive }) => {
-      console.log("onLeaveBack", progress, direction, isActive);      
-      gsap.fromTo(contentElements, { y: 0, opacity: 1 }, { y: -80, opacity: 0, stagger: 0.05 });
-    },
-    onEnterBack: ({ progress, direction, isActive }) => {
-      console.log("onEnterBack", progress, direction, isActive);
-      gsap.fromTo(contentElements, { y: -80, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.05 });
-    }
-  });
-});
+    gsap.to(".owlHorned", {
+      yPercent: 20,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterGreat",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".caption", {
+      yPercent: 100,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterGreat",
+        end: "bottom center",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".clusterGreat", {
+      yPercent: -5,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterGreat",
+        end: "bottom center",
+        scrub: 1
+      },
+    });
+
+    /* ------Burrowing Owl Sequence------ */
+    gsap.set(".triangle", { yPercent: 25, rotation: -90 });
+    gsap.set(".dotsWhite", { yPercent: 10 });
+    gsap.set(".owlBurrowing", { yPercent: -20 });
+    gsap.set(".clusterBurrowing", { yPercent: 5 });
+
+    gsap.to(".triangle", {
+      yPercent: -5,
+      rotation: 40,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterBurrowing",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".dotsWhite", {
+      yPercent: -10,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterBurrowing",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".owlBurrowing", {
+      yPercent: 20,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterBurrowing",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".captionBurrowing", {
+      yPercent: 200,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterBurrowing",
+        end: "bottom center",
+        scrub: 1
+      },
+    });
+
+    gsap.to(".clusterBurrowing", {
+      yPercent: -5,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".clusterBurrowing",
+        end: "bottom center",
+        scrub: 1
+      },
+    });
