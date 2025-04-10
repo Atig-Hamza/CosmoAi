15863c23facc14c92a7442e21dbaa717cde6e886
Atig-Hamza <hamzaatig@icloud.com>
2025-04-10 17:25:22 +0000

    link home page with login page

--- CosmoAI/resources/views/Home.blade.php
+++ CosmoAI/resources/views/Home.blade.php
@@ -152,7 +152,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg> </button>
-                <a href="#"
+                <a href="/login"
                     class="bg-gray-800 hover:bg-gray-700 text-white text-xs py-1.5 px-3 rounded-md transition-colors">Log
                     In</a>
                 <button class="md:hidden text-gray-400 hover:text-white"> <svg xmlns="http://www.w3.org/2000/svg"
