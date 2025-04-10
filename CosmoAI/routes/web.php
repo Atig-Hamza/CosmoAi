9c4ff9f379a5ee864e9b223087f2f217661876a8
Atig-Hamza <hamzaatig@icloud.com>
2025-04-10 11:19:17 +0000

    add all routes

--- CosmoAI/routes/web.php
+++ CosmoAI/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/login', function () {
+    return view('Login');
+});
+
+Route::get('/signup', function () {
+    return view('Signup');
+});
+
+Route::get('/chat', function () {
     return view('Chat');
 });
