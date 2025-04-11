46e82ea4060b544a37276710659ba0ef565fba21
Atig-Hamza <hamzaatig@icloud.com>
2025-04-11 14:56:39 +0000

    Route: add middleware to questionnaire

--- CosmoAI/routes/web.php
+++ CosmoAI/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use GuzzleHttp\Middleware;
+use App\Http\Middleware\VerifyQuestionnaireCompletion;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,7 +22,7 @@ Route::get('/chat', function () {
 
 Route::get('/questionnaire', function () {
     return view('Questionnaire');
-})->name('questionnaire')->middleware('auth');
+})->name('questionnaire')->middleware('auth')->middleware(VerifyQuestionnaireCompletion::class);
 
 Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
 
