a8c85a1d9bd52fe2d60e7149d21f050ea596d719
Atig-Hamza <hamzaatig@icloud.com>
2025-04-28 12:20:06 +0000

    Controller: make first controller function for tickets about opening ticket

---
+++ CosmoAI/app/Http/Controllers/TicketsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tickets;
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller
+{
+    public function openTicket(Request $request, Tickets $ticket) {
+        $request->validate([
+            'problem' => 'required',
+            'email' => 'required',
+            'attachment' => 'nullable|file|mimes:jpeg,png,jpg,pdf|max:2048',
+        ]);
+
+        $ticket->ticket_id = '#Ticket-' . \Str::random(12);
+
+        $ticket->problem = $request->problem;
+        $ticket->email = $request->email;
+
+        $ticket->user_id = auth()->user()->id;
+
+        if ($request->hasFile('attachment')) {
+            $file = $request->file('attachment');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('attachments', $fileName, 'public');
+            $ticket->attachment = $filePath;
+        }
+
+        $ticket->save();
+
+    }
+}
