8b9b473b31b0c5c57159ab8652cde79010964cda
Atig-Hamza <hamzaatig@icloud.com>
2025-05-03 12:50:11 +0000

    model: create plans model

---
+++ CosmoAI/app/Models/Plans.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plans extends Model
+{
+    protected $table = 'plans';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
+
+
+}
